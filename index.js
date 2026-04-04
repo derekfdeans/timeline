2a1793904479e4078c9061073a92d8d0d34901a5
Derek Deans <derekfdeans@icloud.com>
2026-04-04 19:16:53 -0400

    wired up the "remove" button for each event; much cleaner localstorage usage without bloating up my page!

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ html_form.addEventListener("submit", function (event) {
         description: formData.get('event_description'),
     }
 
-    localStorage.setItem(`${formData.get('event_name')}`, JSON.stringify(currentEvent));
+    localStorage.setItem(`${currentEvent.name}`, JSON.stringify(currentEvent));
+    console.log(`added new event: ${currentEvent.name}`)
 
     updateEventList();
 
@@ -31,10 +32,28 @@ function updateEventList() {
         newHTML += `<div>
             <h1>${event.name}</h1>
             <p>${event.description}</p>
+            <button id="${event.name}" class="remove_event">remove event</button>
         </div>`;
     })
 
     html_event_list.innerHTML = newHTML;
+    wireUpButtons();
+}
+
+function wireUpButtons() {
+    let removeEventButtons = document.getElementsByClassName("remove_event");
+
+    for (let button of removeEventButtons) {
+        button.addEventListener("click", function (event) {
+            event.preventDefault();
+
+            localStorage.removeItem(`${button.id}`);
+            console.log(`removed event: ${button.id}`);
+
+            updateEventList();
+
+        });
+    }
 }
 
 setup()
