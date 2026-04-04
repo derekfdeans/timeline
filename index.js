8ba475c18f6ac28a14b74dbdf37a3f4d12f50bec
Derek Deans <derekfdeans@icloud.com>
2026-04-04 18:41:42 -0400

    shifted away from local array of objects to localstorage, successfully adds "events" to localstorage and then reads from it to update the event list

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 console.log('Happy developing ✨')
 
-let list_of_events = [];
+function setup() {
+    updateEventList();
+}
 
 const html_form = document.getElementById("add_event");
 html_form.addEventListener("submit", function (event) {
     event.preventDefault()
 
     const formData = new FormData(html_form);
-    list_of_events.push({
-        eventName: formData.get("event_name"),
-        eventDesc: formData.get("event_description"),
-    },);
 
-    console.log(list_of_events);
+    let currentEvent = {
+        name: formData.get('event_name'),
+        description: formData.get('event_description'),
+    }
+
+    localStorage.setItem(`${formData.get('event_name')}`, JSON.stringify(currentEvent));
+
     updateEventList();
 
     html_form.reset();
@@ -21,13 +25,16 @@ html_form.addEventListener("submit", function (event) {
 const html_event_list = document.getElementById("event_holder");
 function updateEventList() {
     let newHTML = "";
-    for (let event of list_of_events) {
+    Object.keys(localStorage).forEach(key => {
+        let event = JSON.parse(localStorage[key]);
+
         newHTML += `<div>
-        <h1>${event.eventName}</h1>
-        <p>${event.eventDesc}</p>
-    </div>`;
-    }
+            <h1>${event.name}</h1>
+            <p>${event.description}</p>
+        </div>`;
+    })
 
     html_event_list.innerHTML = newHTML;
 }
 
+setup()
