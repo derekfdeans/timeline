4c9e2ca51c029aab196ca9f1e9cfb815204f8991
Derek Deans <derekfdeans@icloud.com>
2026-04-04 22:30:48 -0400

    little const/reordering tweaks

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 console.log('Happy developing ✨')
 
-/* FOR THE FULL REFACTOR
-
-first off: try to clean up the id situation where everything is essentially just {event.name} wrapped in a different way. use a unique id, like date.now() (suggested by claude) for unique keys that don't actually need to be used by me! just keeping track of events
-
-another: separate out functions more; loadEventList shouldn't also handle adding a new div node. bring out the div node so that it can be recycled by the html form to add a task to the list
-
- */
-
 function setup() {
     loadEventList();
 }
 
 function loadEventList() {
-    let allTasks = Object.keys(localStorage);
+    const allTasks = Object.keys(localStorage);
     allTasks.forEach(key => {
         addToTaskList(key);
     })
@@ -25,13 +17,14 @@ addEventForm.addEventListener("submit", function (task) {
 
     const formData = new FormData(addEventForm);
 
+    const key = String(Date.now());
+
     let currentTask = {
+        id: key,
         name: formData.get('eventName'),
         description: formData.get('taskDescription'),
         completed: false,
     }
-
-    let key = String(Date.now());
 
     localStorage.setItem(key, JSON.stringify(currentTask));
     addToTaskList(key);
@@ -40,18 +33,20 @@ addEventForm.addEventListener("submit", function (task) {
 })
 
 function addToTaskList(key) {
-    let event = JSON.parse(localStorage[key]);
-    let divPiece = event.completed ? `<div class="completed">` : `<div>`;
+    const event = JSON.parse(localStorage[key]);
+    const divPiece = event.completed ? `<div class="completed">` : `<div>`;
 
     const container = document.getElementById("container");
+    const div = document.createElement("div");
 
-    container.innerHTML += `${divPiece}` +
+    div.innerHTML = `${divPiece}` +
         `<h1>${event.name}</h1>
             <p>${event.description}</p>
             <button id="${key}-remove" class="removeTask">remove event</button>
             <button id="${key}-complete" class="completeTask">complete event</button>
         </div>`;
 
+    container.appendChild(div);
     wireCompleteButton(key);
     wireRemoveButton(key);
 }
@@ -59,7 +54,7 @@ function addToTaskList(key) {
 function wireCompleteButton(key) {
     let button = document.getElementById(`${key}-complete`);
     button.addEventListener("click", function () {
-        let eventObject = JSON.parse(localStorage.getItem(key));
+        const eventObject = JSON.parse(localStorage.getItem(key));
 
         eventObject.completed = true;
         localStorage.setItem(key, JSON.stringify(eventObject));
